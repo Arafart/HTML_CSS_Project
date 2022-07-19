c10267d76479c2a9d56130ea719e64049667c1f8
Arafat <arafatalam.9616@gmail.com>
2022-07-19 12:10:33 +0400

    HTML_CSS_project

---
+++ food/js/aap.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.food-slider').slick({
+        autoplay: true,
+        slidesToShow:3,
+        slidesToScrool:1,
+        prevArrow: ".prev-btn",
+        nextArrow: ".next-btn",
+    });
+});
